6cec39d6445ad94124fa62176f68455ff307a791
Ameed Houssen <ameedhousen@gmail.com>
2026-06-04 15:51:25 +0300

    Create User_Setup.h

---
+++ ESP32/SmartFridge_ESP32_CH/User_Setup.h
@@ -0,0 +1,25 @@
+#define USER_SETUP_INFO "SmartFridge_Display"
+
+#define ILI9488_DRIVER
+
+#define TFT_MOSI  23
+#define TFT_SCLK  18
+#define TFT_MISO  19
+#define TFT_CS    -1
+#define TFT_DC    27
+#define TFT_RST    4
+
+#define TOUCH_CS   5
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+#define SMOOTH_FONT
+
+#define SPI_FREQUENCY        10000000
+#define SPI_READ_FREQUENCY   10000000
+#define SPI_TOUCH_FREQUENCY   2500000
